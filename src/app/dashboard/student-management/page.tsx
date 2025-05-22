76445bacc2d053fa64cb80aa633bd8111d8ff773
andersongil <andersongil.dev@gmail.com>
2025-05-22 18:25:42 -0400

    table for student list with tailwind styles

--- src/app/dashboard/student-management/page.tsx
+++ src/app/dashboard/student-management/page.tsx
@@ -1,3 +1,33 @@
+const mosckStudents = [];
+
 export default function Page() {
-    return <p>Student Management Page!</p>;
+	return (
+		<>
+			<h2 className="text-lg font-semibold text-center">Management</h2>
+			<table className="mt-9 w-3/5 mx-auto table-auto border-collapse text-sm">
+				<thead>
+					<tr className="border-b border-gray-200">
+						<th className=" table-title">Name</th>
+						<th className="table-title">Birthdate</th>
+						<th className="table-title">Course?</th>
+						<th className="table-title">Teacher</th>
+						<th className="table-title">Phone Number</th>
+						<th className="table-title">Parent</th>
+						<th className="table-title">Parent Email</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr className="border-b border-gray-200">
+						<td className="table-title py-3">pedro</td>
+						<td className="table-title py-3">2005-02-6</td>
+						<td className="table-title py-3">Math101</td>
+						<td className="table-title py-3">steve jobs</td>
+						<td className="table-title py-3">+984125257172</td>
+						<td className="table-title py-3">pedro parent</td>
+						<td className="table-title py-3">pedro@pedro.com</td>
+					</tr>
+				</tbody>
+			</table>
+		</>
+	);
 }
