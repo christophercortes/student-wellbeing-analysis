136929648a19c9385bb7c3b99c703a546c238f5c
Christopher Cortes <christopherctes@hotmail.com>
2025-05-22 16:41:52 -0400

    clean empty var

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Schema, model } from 'mongoose';
+import { Document, Schema, model } from 'mongoose';
 
 export interface IStudent extends Document {
   fullName: string;
