88ee7e4632abb080bb6aa5e0dbf3c8a557c448d2
andersongil <andersongil.dev@gmail.com>
2025-06-04 13:33:48 -0400

    Changing input for file submit

--- src/components/SentimentAnalysis.jsx
+++ src/components/SentimentAnalysis.jsx
@@ -12,6 +12,7 @@ export const SentimentAnalysis = () => {
 	const [loading, setLoading] = useState(false);
 	const [vaderResult, setVaderResult] = useState(null);
 	const [error, setError] = useState(null);
+	const [fileName, setFileName] = useState("");
 
 	// Create a reference to the worker object.
 	const worker = useRef(null);
@@ -73,6 +74,22 @@ export const SentimentAnalysis = () => {
 			worker.current.postMessage({ text });
 		}
 	}, []);
+
+	const handleFileChange = (e) => {
+		const file = e.target.files[0];
+		if (file && file.type === "text/plain") {
+			setFileName(file.name);
+			const reader = new FileReader();
+			reader.onload = (event) => {
+				const text = event.target.result;
+				classify(text);
+			};
+			reader.readAsText(file);
+		} else {
+			setError("Please select a valid .txt file");
+		}
+	};
+
 	let summary = null;
 	let classNames = null;
 	if (result) {
@@ -121,14 +138,43 @@ export const SentimentAnalysis = () => {
 			<h1 className="text-5xl font-bold mb-2 text-center">Emotion Analysis</h1>
 			<h2 className="text-2xl mb-4 text-center">Powered by Hugging Face</h2>
 
-			<input
-				className="w-full max-w-xs p-2 border border-gray-300 rounded mb-4"
-				type="text"
-				placeholder="Enter text here"
-				onInput={(e) => {
-					classify(e.target.value);
-				}}
-			/>
+			<div className="w-full max-w-xs">
+				<label className="flex flex-col items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100">
+					<div className="flex flex-col items-center justify-center pt-5 pb-6">
+						<svg
+							className="w-8 h-8 mb-4 text-gray-500"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 20 16"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+							/>
+						</svg>
+						<p className="mb-2 text-sm text-gray-500">
+							<span className="font-semibold">Click to upload</span> or drag and
+							drop
+						</p>
+						<p className="text-xs text-gray-500">.TXT files only</p>
+					</div>
+					<input
+						type="file"
+						className="hidden"
+						accept=".txt"
+						onChange={handleFileChange}
+					/>
+				</label>
+				{fileName && (
+					<p className="mt-2 text-sm text-gray-500 text-center">
+						Selected file: {fileName}
+					</p>
+				)}
+			</div>
 
 			{renderResults()}
 		</div>
