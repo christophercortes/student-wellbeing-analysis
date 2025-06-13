da43ad356524b2a2e3bb612d1207389bc8af4b5a
byui_student1045 <sco22022@byui.edu>
2025-06-13 01:17:17 -0600

    Updated teacher info view

--- src/app/dashboard/user-profile/page.tsx
+++ src/app/dashboard/user-profile/page.tsx
@@ -1,6 +1,6 @@
 /*
     This page displays the teacher information and also allows for the user
-    information to be editied by the user if needed.
+    information.
  */
 // Use Client to use react
 "use client";
@@ -12,11 +12,11 @@ import { useSession } from "next-auth/react"; // Obtain the session to use in th
 export default function UserProfile()
 {
     // Obtain the data inside the session
-    const { data: session } = useSession(); // Comment this out for a second while you test this page, it should work because of what is imported
+    const { data: Session } = useSession(); // Comment this out for a second while you test this page, it should work because of what is imported
 
     // Return the rendered page
     return (<>
         <h2>Hello This Is the Teacher Info Page.</h2>
-        <h2>{session ? (<p>The id: {session.user.id}</p>) : (<p>Please Log in.</p>)}</h2>
+        <h2>{Session ? (<p>The id: {Session.user.id}</p>) : (<p>Please Log in.</p>)}</h2>
     </>);
 }
